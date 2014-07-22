f13f50f213987590bec2c346c7ecbe2e96a0391a
Leandra Tejedor <lmtejedor@gmail.com>
2014-07-22 10:52:49 -0400

    Add admin users option to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+users = {
+
+    admin: {
+
+        name: 'admin',
+        email: 'admin@gmail.com',
+        password: 'adminpass',
+        password_confirmation: 'adminpass',
+        admin: true
+    },
+
+    administrator: {
+
+        name: 'administrator',
+        email: 'administrator@gmail.com',
+        password: 'administrator',
+        password_confirmation: 'administrator',
+        admin: true
+    }
+}
+
+users.each do |user, data|
+
+  user = User.new(data)
+
+  unless User.where(email: user.email).exists?
+    user.save!
+  end
+end
